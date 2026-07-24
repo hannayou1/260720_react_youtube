2231346607a2ebc8f2913ac96bd34d30626f1095
hannayou1 <pooh9868@naver.com>
2026-07-24 18:00:00 +0900

    15강 비디오 컴퍼넌트 통합 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,12 @@ import Gsap from "../components/contents/Gsap";
 import Portfolio from "../components/contents/Portfolio";
 import Youtube from "../components/contents/Youtube";
 import Website from "../components/contents/Website";
+import VideoSlider02 from "../components/video/VideoSlider02";
+import { webdText } from "../data/webd";
+import { websiteText } from "../data/website";
+import { gsapText } from "../data/gsap";
+import { portfolioText } from "../data/portfolio";
+import { youtubeText } from "../data/youtube";
 
 
 const Home = () => {
@@ -14,11 +20,11 @@ const Home = () => {
     <Main title="Hanna 유튜브" description="한나 유튜브에 오신 것을 환영합니다♥">
       <Today/>
       <Developer/>
-      <Webd/>
-      <Website/>
-      <Gsap/>
-      <Portfolio/>
-      <Youtube/>
+      <VideoSlider02 videos={webdText} title="웹디자인 준비는 이걸로" id="webd" />
+      <VideoSlider02 videos={websiteText} title="웹사이트" id="website" />
+      <VideoSlider02 videos={gsapText} title="gsap" id="gsap" />
+      <VideoSlider02 videos={portfolioText} title="포트폴리오 " id="website"  />
+      <VideoSlider02 videos={youtubeText} title="유튜브" id="website"  />
     </Main>
   );
 };
