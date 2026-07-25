dca469e88b720280deff9f3a49508371ac65b950
hannayou1 <pooh9868@naver.com>
2026-07-25 16:12:01 +0900

    18강 검색 및 유튜브 API 설정하기

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -3,7 +3,7 @@ import Main from '../components/section/Main'
 
 import { todayText } from "../data/today"
 import { Link } from "react-router-dom"
-import { use } from 'react'
+
 
 const Today = () => {
     const [loading, setLoading] = useState(true);
