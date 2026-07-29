3fe749a47665763c99fc6c68e3662e545d790a05
hannayou1 <pooh9868@naver.com>
2026-07-29 17:34:26 +0900

    20강 비디오 상세 페이지 만들기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -42,6 +42,7 @@ const Search = () => {
   return (
     <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
       <section id="searchPage" className={searchPageClass}>
+        <h2>★ <em>{searchId}</em> 검색 결과 입니다.</h2>
         <div className="video__inner search">
           <VideoSearch videos={videos} />
         </div>
